093b064d87498c24472fa55c0b39c7c270e0e23d
Jeff Truitt <jtruitt@gmail.com>
2022-07-09 00:15:17 -0700

    adding places controller

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 let express = require('express')
 let app = express()
+let PORT = process.env.PORT
 
 app.get('/', (req, res) => {
-    res.send('Hello world!!!!')
+    res.send('Hello world!')
 })
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page hit</h1>')
 })
 
-app.listen(process.env.PORT)
+app.listen(PORT, ()=>{
+    console.log(`monitoring ${PORT}`)
+})
