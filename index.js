6fd77bf2d122b77c182a4b21eb495d3a163327f9
Jeff Truitt <jtruitt@gmail.com>
2022-07-09 01:48:41 -0700

    adding home page view

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ let express = require('express')
 let app = express()
 let PORT = process.env.PORT
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places.js'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page hit</h1>')
---
+++ views/home.jsx
@@ -0,0 +1,14 @@
+let React = require('react')
+let Def = require('./default.jsx')
+
+function home(){
+    return(
+        <Def>
+            <main>
+                <h1>HOME is where the heart is</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = home
