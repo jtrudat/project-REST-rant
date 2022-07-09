90d59b05df3b832e192e9602b05380254395adc8
Jeff Truitt <jtruitt@gmail.com>
2022-07-08 23:51:09 -0700

    404 routing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+let express = require('express')
+let app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello world!!!!')
 })
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page hit</h1>')
+})
 
-app.listen(3000)
+app.listen(process.env.PORT)
