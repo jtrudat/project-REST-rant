354ecff4bb96bc143c8341fd726ddd5d7547ebcf
Jeff Truitt <jtruitt@gmail.com>
2022-07-11 00:43:39 -0700

    adding styles to Rest-Rant

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//Modules and Globals
 require('dotenv').config()
 let express = require('express')
 let app = express()
 let PORT = process.env.PORT
 
+//Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//Controllers and routes
 app.use('/places', require('./controllers/places.js'))
 
 app.get('/', (req, res) => {
@@ -15,6 +20,7 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+//Listen for connections to route
 app.listen(PORT, ()=>{
     console.log(`monitoring ${PORT}`)
 })
