2780ffc1f802325af896d24238d3c9a63fb9401f
Jeff Truitt <jtruitt@gmail.com>
2022-07-11 19:56:01 -0700

    show connected route

---
+++ views/places/show.jsx
@@ -0,0 +1,14 @@
+let React = require('react')
+let Def = require('../default')
+
+function show () {
+    return (
+        <Def>
+          <main>
+            <h1>Show Page</h1>
+          </main>
+        </Def>
+    )
+}
+
+module.exports = show
