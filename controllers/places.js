9652d671c45cea0e5fdc82ca872678843e3285fd
Jeff Truitt <jtruitt@gmail.com>
2022-07-17 19:16:18 -0700

    connected to db with index, create and posting

--- controllers/places.js
+++ controllers/places.js
@@ -1,99 +1,63 @@
 let router = require('express').Router()
-let places = require('../models/places.js')
+let db = require('../models')
+
 //GET /places (route to the list of all places)
 router.get('/', (req, res)=>{
-    console.log('arrived at subcategory places')
-    res.render('places/index', {places})
+    db.Place.find()
+    .then((places)=>{
+      res.render('places/index', {places})
+    })
+    .catch(err =>{
+      console.log(err),
+      res.render('error404')
+    })
 })
 
 //Creating pt2 Adding NEWLY created places (routing a newly created place to the list of all places)
 router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-        // Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-        req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-        req.body.state = 'USA'
-      }
-    places.push(req.body)
-    res.redirect('/places')
+    db.Place.create(req.body)
+    .then(()=>{
+      res.redirect('/places')
+    })
+    .catch(err=>{
+      console.log('err', err)
+      res.render('error404')
+    })
   })
 
 //Creating pt1 NEW places (the place where you go to fill in the template of a new place)
 router.get('/new', (req, res) => {
-    res.render('places/new.jsx')
+    res.render('places/new')
   })
   
 //SHOW (routing to the page displaying a particular item menu in detail (/:id = places/id index number) )
 router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]){
-        res.render('error404')
-    }
-    else {
-      res.render('places/show', {place: places[id], id})
-    }
-    console.log(places[id])
+    res.send('GET /places/:id route stub good check')
   })
  
 //EDIT pt2 (then reposting that items index with changes)  
   router.put('/:id', (req, res) => {
-    
-    let id = Number(req.params.id)
-    if (isNaN(id)){
-        res.render('error404')
-    }
-    else if (!places[id]){
-        res.render('error404')
-    }
-    else {
-        if (!req.body.pic) {
-            req.body.pic = 'http://placekitten.com/400/400'
-        }
-        if (!req.body.city) {
-            req.body.city = 'Anytown'
-        }
-        if (!req.body.state) {
-            req.body.state = 'USA'
-        }
-        places[id] = req.body
-        res.redirect(`/places/${id}`)
-    }
+    res.send('PUT /places/:id route stub good check')
 })
 
 //DELETE (routing to remove an itme by its index)
 router.delete('/:id', (req, res) => {
-    let id =Number(req.params.id)
-    if(isNaN(id)){
-        res.render('error404')
-    }
-    else if (!places[id]){
-        res.render('error404')
-    }
-    else {
-        places.splice(id, 1)
-        res.redirect('/places')
-    }
+    res.send('DELETE /places/:id route stub good check')
 })  
 
 //EDIT pt 1 (routing to a specific item, make changes) 
 router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id)
-    if(isNaN(id)){
-        res.render('error404')
-    }
-    else if (!places[id]){
-        res.render('error404')
-    }
-    else {
-    res.render('places/edit', {place: places[id], id})
-    }
+    res.send('GET /places/:id/edit form route stub good check')
 })
+
+//POST RANTS routing
+router.post('/:id/rant', (req, res)=>{
+  res.send('GET /places/:id/rant route stub good check')
+})
+
+//DELETE RANTS routing
+router.delete('/:id/rant/:rantId', (req, res)=>{
+  res.send('DELETE /places/:id/rant/:rantId route stub good check')
+})
+
 module.exports = router
