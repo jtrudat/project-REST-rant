481034bea6de2ac449f8de40f1f5cd150fd095ba
Jeff Truitt <jtruitt@gmail.com>
2022-07-19 23:52:11 -0700

    updated home landing image.complete through pt8

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home(){
             <main>
                 <h1>HOME is where the heart is</h1>
                 <div>
-                    <img src="images/veggies.jpg" alt="veggies"></img>
+                    <img src="https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=755&q=80" alt="veggies"></img>
                 </div>
                 <div>
                 <h3>Photo by <a href="https://unsplash.com/es/@danielcgold?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Dan Gold</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></h3>
